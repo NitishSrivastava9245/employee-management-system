99c70a59f774291c22c2b76b6fece86dd142cd25
hp <hp@192.168.1.33>
2026-01-18 14:53:18 +0530

    Initial commit - Employee Management System

---
+++ employee-management-system/src/main/java/com/ems/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.ems.controller;
+
+import com.ems.model.Employee;
+import com.ems.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+}
